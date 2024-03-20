ef06d76fd1b2deee6bcf6af3a92696da5c7a5017
Sina jeirani <104722946+SinaJry@users.noreply.github.com>
2024-03-20 18:53:19 +0330

    Update ps_crawler.py

--- ps_crawler.py
+++ ps_crawler.py
@@ -1,23 +1,25 @@
 import requests
+import grequests
 from bs4 import BeautifulSoup as bs
 import time
 from tqdm import tqdm
 import sqlite3
 import pandas as pd
 import itertools
-import  cProfile
 import lxml
-import grequests
+
 
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
 url = r'https://store.playstation.com/en-us/category/dc464929-edee-48a5-bcd3-1e6f5250ae80'
 
+#Getting All Available Page URLs to Crawl
 def get_urls():
     urls = []
     
     r= requests.get(url,headers = headers)
     soup = bs(r.content,'html.parser')
+    #last available page number
     value = soup.find_all(class_='psw-button psw-b-0 psw-page-button psw-p-x-3 psw-r-pill psw-l-line-center psw-l-inline psw-t-size-3 psw-t-align-c')[-1].text
     
     for i in range(1,int(value)+1):
@@ -25,13 +27,13 @@ def get_urls():
     
     return urls
 
-
+#Async Requesting
 def fetch_data(urls):
     reqs = [grequests.get(url) for url in urls]
     resp = grequests.map(reqs)
     return resp
 
-
+#Parsing Item with Soup (lxml parser make it faster!)
 def parser(resp):
     items = []
     for r in resp:
@@ -39,6 +41,7 @@ def parser(resp):
         all_games = soup.find_all('li',class_= "psw-l-w-1/2@mobile-s psw-l-w-1/2@mobile-l psw-l-w-1/6@tablet-l psw-l-w-1/4@tablet-s psw-l-w-1/6@laptop psw-l-w-1/8@desktop psw-l-w-1/8@max")
         for game in all_games:
             item = {'name':game.find(class_ = 'psw-t-body psw-c-t-1 psw-t-truncate-2 psw-m-b-2').text}
+            #Some Game Items Doesnt Have any further information except to their names
             try:
                 item['newprice'] = game.find(class_ = 'psw-m-r-3').text.replace('$', '') + '$'
                 item['oldprice'] = game.find('s' , class_ = 'psw-c-t-2').text.replace('$', '') + '$'
@@ -61,7 +64,7 @@ def parser(resp):
     return data
 
 
-
+#Making Connection to a database and inserting crawled data into it!
 def db_conn():
     
     conn =sqlite3.Connection(r'example.db')
